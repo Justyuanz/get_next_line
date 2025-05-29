855206102ea0720aeb5d9ca9bd39254ce20cd3a9
jinzhang <jinzhang@hive.student.fi>
2025-05-30 01:37:52 +0300

    header

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,13 @@
 /*   By: jinzhang <jinzhang@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 20:18:34 by jinzhang          #+#    #+#             */
-/*   Updated: 2025/05/30 01:16:33 by jinzhang         ###   ########.fr       */
+/*   Updated: 2025/05/30 01:37:23 by jinzhang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdio.h> //DELETE ME
 # include <unistd.h>
 # include <stdlib.h>
 
@@ -31,12 +30,10 @@ typedef struct s_gnl
 } t_gnl;
 
 char	*get_next_line(int fd);
-void 	gnl_free(char **ptr);
-int 	gnl_find_nl(char	*buf, char c);
 char	*gnl_strjoin(char *pre_line, char *buf, int nl);
-char	*gnl_substr(char *src, int i, int nl);
-int gnl_updatebuf(char *buf);
-int gnl_strlen(char *str);
-char *gnl_error_eof(t_gnl *gnl, char *buf);
+char 	*gnl_error_eof(t_gnl *gnl, char *buf);
+int 	gnl_find_nl(char	*buf, char c);
+int 	gnl_updatebuf(char *buf);
+int 	gnl_strlen(char *str);
 
 #endif
